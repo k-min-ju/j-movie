6c41dc2117b70d02d85df975361ad9a3e3dcac9a
mjkim <alswn8065@gmail.com>
2024-09-11 21:54:06 +0900

    Revise remove duplicate imports

---
+++ src/reducer/index.js
@@ -0,0 +1,41 @@
+import actionReducer from '@/reducer/actionReducer.js';
+import adventureReducer from '@/reducer/adventureReducer.js';
+import animationReducer from '@/reducer/animationReducer.js';
+import comedyReducer from '@/reducer/comedyReducer.js';
+import crimeReducer from '@/reducer/crimeReducer.js';
+import dramaReducer from '@/reducer/dramaReducer.js';
+import familyReducer from '@/reducer/familyReducer.js';
+import highteenReducer from '@/reducer/highteenReducer.js';
+import horrorReducer from '@/reducer/horrorReducer.js';
+import lastYearReducer from '@/reducer/lastYearReducer.js';
+import meloReducer from '@/reducer/meloReducer.js';
+import movieSearchReducer from '@/reducer/movieSearchReducer.js';
+import mysteryReducer from '@/reducer/mysteryReducer.js';
+import recentReleaseReducer from '@/reducer/recentReleaseReducer.js';
+import romanceReducer from '@/reducer/romanceReducer.js';
+import sfReducer from '@/reducer/sfReducer.js';
+import specialReducer from '@/reducer/specialReducer.js';
+import thrillerReducer from '@/reducer/thrillerReducer.js';
+import youthReducer from '@/reducer/youthReducer.js';
+
+export {
+  actionReducer,
+  adventureReducer,
+  animationReducer,
+  comedyReducer,
+  crimeReducer,
+  dramaReducer,
+  familyReducer,
+  highteenReducer,
+  horrorReducer,
+  lastYearReducer,
+  meloReducer,
+  movieSearchReducer,
+  mysteryReducer,
+  recentReleaseReducer,
+  romanceReducer,
+  sfReducer,
+  specialReducer,
+  thrillerReducer,
+  youthReducer
+};
--- src/store.js
+++ src/store.js
@@ -1,44 +1,46 @@
-import {configureStore} from "@reduxjs/toolkit";
-import lastYearReducer from './reducer/lastYearReducer.js';
-import recentReleaseReducer from "./reducer/recentReleaseReducer.js";
-import animationReducer from "./reducer/animationReducer.js";
-import crimeReducer from "./reducer/crimeReducer.js";
-import thrillerReducer from "./reducer/thrillerReducer.js";
-import dramaReducer from "./reducer/dramaReducer.js";
-import sfReducer from "./reducer/sfReducer.js";
-import specialReducer from "./reducer/specialReducer.js";
-import actionReducer from "./reducer/actionReducer.js";
-import adventureReducer from "./reducer/adventureReducer.js";
-import comedyReducer from "./reducer/comedyReducer.js";
-import familyReducer from "./reducer/familyReducer.js";
-import highteenReducer from "./reducer/highteenReducer.js";
-import horrorReducer from "./reducer/horrorReducer.js";
-import meloReducer from "./reducer/meloReducer.js";
-import mysteryReducer from "./reducer/mysteryReducer.js";
-import romanceReducer from "./reducer/romanceReducer.js";
-import youthReducer from "./reducer/youthReducer.js";
-import movieSearchReducer from "./reducer/movieSearchReducer.js";
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  actionReducer,
+  adventureReducer,
+  animationReducer,
+  comedyReducer,
+  crimeReducer,
+  dramaReducer,
+  familyReducer,
+  highteenReducer,
+  horrorReducer,
+  lastYearReducer,
+  meloReducer,
+  movieSearchReducer,
+  mysteryReducer,
+  recentReleaseReducer,
+  romanceReducer,
+  sfReducer,
+  specialReducer,
+  thrillerReducer,
+  youthReducer
+} from '@/reducer';
 
 export default configureStore({
-    reducer: {
-        lastYearReducer : lastYearReducer.reducer,
-        recentReleaseReducer: recentReleaseReducer.reducer,
-        animationReducer: animationReducer.reducer,
-        crimeReducer: crimeReducer.reducer,
-        thrillerReducer: thrillerReducer.reducer,
-        dramaReducer: dramaReducer.reducer,
-        sfReducer: sfReducer.reducer,
-        specialReducer: specialReducer.reducer,
-        actionReducer: actionReducer.reducer,
-        adventureReducer: adventureReducer.reducer,
-        comedyReducer: comedyReducer.reducer,
-        familyReducer: familyReducer.reducer,
-        highteenReducer: highteenReducer.reducer,
-        horrorReducer: horrorReducer.reducer,
-        meloReducer: meloReducer.reducer,
-        mysteryReducer: mysteryReducer.reducer,
-        romanceReducer: romanceReducer.reducer,
-        youthReducer: youthReducer.reducer,
-        movieSearchReducer: movieSearchReducer.reducer,
-    }
-})
+  reducer: {
+    lastYearReducer: lastYearReducer.reducer,
+    recentReleaseReducer: recentReleaseReducer.reducer,
+    animationReducer: animationReducer.reducer,
+    crimeReducer: crimeReducer.reducer,
+    thrillerReducer: thrillerReducer.reducer,
+    dramaReducer: dramaReducer.reducer,
+    sfReducer: sfReducer.reducer,
+    specialReducer: specialReducer.reducer,
+    actionReducer: actionReducer.reducer,
+    adventureReducer: adventureReducer.reducer,
+    comedyReducer: comedyReducer.reducer,
+    familyReducer: familyReducer.reducer,
+    highteenReducer: highteenReducer.reducer,
+    horrorReducer: horrorReducer.reducer,
+    meloReducer: meloReducer.reducer,
+    mysteryReducer: mysteryReducer.reducer,
+    romanceReducer: romanceReducer.reducer,
+    youthReducer: youthReducer.reducer,
+    movieSearchReducer: movieSearchReducer.reducer
+  }
+});
