327d42f7d3fafccc2b87670743a9d6bf8a10a834
mjkim <alswn8065@gmail.com>
2024-09-11 22:58:10 +0900

    Add RatingIcon components

---
+++ src/components/icon/index.js
@@ -0,0 +1,6 @@
+import Rating18Icon from '@/components/icon/Rating18Icon';
+import Rating15Icon from '@/components/icon/Rating15Icon';
+import Rating12Icon from '@/components/icon/Rating12Icon';
+import RatingAllIcon from '@/components/icon/RatingAllIcon';
+
+export { Rating18Icon, Rating15Icon, Rating12Icon, RatingAllIcon };
